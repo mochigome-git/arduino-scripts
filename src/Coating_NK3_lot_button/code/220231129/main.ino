ff57b8c473c97ea688ba588fe33146478183825f
Mochigome <leong.general@gmail.com>
2023-12-01 14:00:59 +0800

    update main code

--- src/Coating_NK3_lot_button/code/220231129/main.ino
+++ src/Coating_NK3_lot_button/code/220231129/main.ino
@@ -4,107 +4,83 @@
 #include <ArduinoHttpClient.h>
 #include "arduino_secrets.h"
 
-// Wi-Fi and MQTT credentials
-char ssid[] = SECRET_SSID; // Network SSID (name)
-char pass[] = SECRET_PASS; // Network password
-int keyIndex = 0;          // Network key index number (needed only for WEP)
-
-int limitSwitch = 13;
+// Define a constant for the pin number
+const int limitSwitchPin = 13;
 int state = LOW;
 
-char serverAddress[] = "192.168.0.126"; // server address
+// Wi-Fi configuration
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int status = WL_IDLE_STATUS;
+const unsigned long interval = 3000;
+unsigned long previousMillis = 0;
+WiFiClient wifi;
+
+// http configuration
+char serverAddress[] = "192.100.0.25"; // server address
 int port = 8000;
 // Specify the complete URL path including the table
-char *urlPath = "/rest/v1/test";
-String HTTP_METHOD = "POST";
-String SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAic2VydmljZV9yb2xlIiwKICAgICJpc3MiOiAic3VwYWJhc2UiLAogICAgImlhdCI6IDE2NjYwMjI0MDAsCiAgICAiZXhwIjogMTgyMzc4ODgwMAp9.sbuBA2BnmzMP1CIMIyPWPEnAkGSnBUhFsOwcXEng5qg";
-
-char *postData = "{\"test\": true}";
-
-// Time interval for connection attempts
-const unsigned long interval = 8000;
-unsigned long previousMillis = 0;
-
-// Create an HTTP client object
-WiFiClient wifi;
+char *urlPath = "/rest/v1/nk3_log_data_realtime?id=eq.1";
+const String HTTP_METHOD = "PATCH";
+const String SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAic2VydmljZV9yb2xlIiwKICAgICJpc3MiOiAic3VwYWJhc2UiLAogICAgImlhdCI6IDE2NjYwMjI0MDAsCiAgICAiZXhwIjogMTgyMzc4ODgwMAp9.sbuBA2BnmzMP1CIMIyPWPEnAkGSnBUhFsOwcXEng5qg";
 HttpClient client = HttpClient(wifi, serverAddress, port);
-int status = WL_IDLE_STATUS;
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(limitSwitch, INPUT);
+    pinMode(limitSwitchPin, INPUT);
+    // check for the WiFi module:
+    if (WiFi.status() == WL_NO_MODULE)
+    {
+        Serial.println("Communication with WiFi module failed!");
+        // don't continue
+        while (true)
+            ;
+    }
+    // attempt to connect to WiFi network:
+    while (status != WL_CONNECTED)
+    {
+        Serial.print("Attempting to connect to WPA SSID: ");
+        Serial.println(ssid);
+        // Connect to WPA/WPA2 network:
+        status = WiFi.begin(ssid, pass);
+    }
 
-    // Connect to Wi-Fi
-    connectToWiFi();
+    // you're connected now, so print out the data:
+    Serial.print("You're connected to the network");
+    printCurrentNet();
+    printWifiData();
 }
 
 void loop()
 {
-    // Check if the Wi-Fi connection is lost
-    if (WiFi.status() != WL_CONNECTED)
+    // Read the limit switch state
+    int val = digitalRead(limitSwitchPin);
+    // Only print if the state changes to HIGH (1)
+    if (val == state)
     {
-        // Attempt to reconnect to Wi-Fi
-        connectToWiFi();
-    }
-    else
-    {
-        int val = digitalRead(limitSwitch);
-
-        if (val != state)
-        {
-            state = val;
-
-            // Make an HTTP POST request when the limit switch state changes
-            if (state == 1)
-            {
-                makeHttpPostRequest();
-                delay(100);
-            }
-        }
+        makeHttpPostRequest("{\"dlot\": 1}");
+        delay(1000);
+        makeHttpPostRequest("{\"dlot\": 0}");
     }
 }
 
-void connectToWiFi()
+void printWiFiStatus()
 {
-    unsigned long currentMillis = millis();
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
 
-    if (currentMillis - previousMillis >= interval)
-    {
-        previousMillis = currentMillis;
-
-        // Check if Wi-Fi is disconnected
-        if (WiFi.status() != WL_CONNECTED)
-        {
-            Serial.print("Attempting to connect to WPA SSID: ");
-            Serial.println(ssid);
-
-            int retryCount = 0;
-            while (WiFi.begin(ssid, pass) != WL_CONNECTED && retryCount < 5)
-            {
-                Serial.print(".");
-                delay(5000);
-                retryCount++;
-            }
-
-            if (WiFi.status() == WL_CONNECTED)
-            {
-                Serial.println("Connected to the network");
-            }
-            else
-            {
-                Serial.println("Failed to connect to the network");
-            }
-        }
-    }
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
 }
 
-void makeHttpPostRequest()
+void makeHttpPostRequest(char *postData)
 {
     // Additional parameters for the request
     const char *contentType = "application/json";
 
-    Serial.println("making POST request");
+    Serial.println("Making POST request");
 
     // Make the HTTP POST request
     client.beginRequest();
@@ -123,7 +99,24 @@ void makeHttpPostRequest()
     // End the request
     client.endRequest();
 
+    // Record the start time
+    unsigned long startTime = millis();
+    // Set the timeout duration (in milliseconds)
+    const unsigned long timeout = 5000;
+
     // Read the status code and body of the response
+    while (!client.available() && (millis() - startTime) < timeout)
+    {
+        delay(10); // Wait for data or timeout
+    }
+
+    // Check if the request timed out
+    if ((millis() - startTime) >= timeout)
+    {
+        Serial.println("HTTP POST request timed out");
+        return;
+    }
+
     int statusCode = client.responseStatusCode();
     String response = client.responseBody();
 
@@ -146,4 +139,60 @@ void makeHttpPostRequest()
     {
         Serial.println("HTTP POST request failed");
     }
+}
+
+void printWifiData()
+{
+    // print your board's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+
+    Serial.println(ip);
+
+    // print your MAC address:
+    byte mac[6];
+    WiFi.macAddress(mac);
+    Serial.print("MAC address: ");
+    printMacAddress(mac);
+}
+
+void printCurrentNet()
+{
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print the MAC address of the router you're attached to:
+    byte bssid[6];
+    WiFi.BSSID(bssid);
+    Serial.print("BSSID: ");
+    printMacAddress(bssid);
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.println(rssi);
+
+    // print the encryption type:
+    byte encryption = WiFi.encryptionType();
+    Serial.print("Encryption Type:");
+    Serial.println(encryption, HEX);
+    Serial.println();
+}
+
+void printMacAddress(byte mac[])
+{
+    for (int i = 5; i >= 0; i--)
+    {
+        if (mac[i] < 16)
+        {
+            Serial.print("0");
+        }
+        Serial.print(mac[i], HEX);
+        if (i > 0)
+        {
+            Serial.print(":");
+        }
+    }
+    Serial.println();
 }
